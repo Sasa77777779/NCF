4b48c1c2863bab58b4138f19580c3a2e8724d072
zhusa <3288856523@qq.com>
2021-11-15 21:05:33 +0800

    change root

--- config.py
+++ config.py
@@ -7,13 +7,15 @@ model = 'NeuMF-end'
 assert model in ['MLP', 'GMF', 'NeuMF-end', 'NeuMF-pre']
 
 # paths
-main_path = '/home/share/guoyangyang/recommendation/NCF-Data/'
+main_path = './NCF-Data/ml-1m'
 
 train_rating = main_path + '{}.train.rating'.format(dataset)
 test_rating = main_path + '{}.test.rating'.format(dataset)
 test_negative = main_path + '{}.test.negative'.format(dataset)
 
+"""
 model_path = './models/'
 GMF_model_path = model_path + 'GMF.pth'
 MLP_model_path = model_path + 'MLP.pth'
 NeuMF_model_path = model_path + 'NeuMF.pth'
+"""
